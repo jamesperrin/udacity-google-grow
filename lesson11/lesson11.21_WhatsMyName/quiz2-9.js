0f064361a64bfd615123c9dbe313eda02806fe17
Perrin, James <james.perrin.va@gmail.com>
2018-01-22 11:20:51 -0800

    Lesson 11.21 initial check-in.

---
+++ lesson11/lesson11.21_WhatsMyName/quiz2-9.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: What's my Name? (2-9)
+ */
+
+/*
+  Directions:
+  Create a variable called fullName and assign it your full name as a string.
+ */
+
+// your code goes here
