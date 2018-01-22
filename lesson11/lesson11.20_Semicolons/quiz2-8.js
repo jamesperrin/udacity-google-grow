7960c0dd94cd8a5a5073bfa53d640f260baf3148
Perrin, James <james.perrin.va@gmail.com>
2018-01-22 11:11:08 -0800

    Lesson 11.20 initial check-in.

---
+++ lesson11/lesson11.20_Semicolons/quiz2-8.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ */
+
+/*
+  Directions:
+  Define two variables called thingOne and thingTwo and assign them values.
+  Print the values of both variables in one console.log statement using concatenation.
+  For example,
+
+    red blue
+
+  where "red" is the value of thingOne and "blue" is the value of thingTwo.
+  Don't forget to use semicolons!
+
+ */
+
+// your code goes here
