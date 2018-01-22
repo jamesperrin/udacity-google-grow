9670feffad18579b17179c225965fafede04fa28
Perrin, James <james.perrin.va@gmail.com>
2018-01-22 09:40:12 -0800

    Lesson 11.12 initial check-in.

---
+++ lesson11/lesson11.12_FavoriteFood/quiz2-3.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Favorite Food (2-3)
+ */
+
+/*
+  Directions:
+  Create a string with the name of your favorite food.
+  The first letter of the string should be capitalized.
+*/
+
+console.log( /* replace this comment with your string */ );
