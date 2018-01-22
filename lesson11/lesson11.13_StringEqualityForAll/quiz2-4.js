317d475a489dfbb664af39f4bd7bcdc6f9e8e74a
Perrin, James <james.perrin.va@gmail.com>
2018-01-22 09:44:12 -0800

    Lesson 11.13 initial check-in.

---
+++ lesson11/lesson11.13_StringEqualityForAll/quiz2-4.js
@@ -0,0 +1,14 @@
+/*
+ * Programming Quiz: String Equality for All (2-4)
+ */
+
+/*
+  Directions:
+  Fix the right side expression so it evaluates to true.
+
+  "ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal"
+ */
+
+// fix the right side of the expression
+var answer = "ALL Strings are CrEaTeD equal" == "All STRINGS are CrEaTED Equal";
+console.log(answer);
