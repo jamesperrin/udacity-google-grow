45b871ee527e88eb6399e84031d9274b63451f45
Perrin, James <james.perrin.va@gmail.com>
2018-01-22 11:32:05 -0800

    Completed lesson 11.23.

--- lesson11/lesson11.23_MadLibs/quiz2-11.js
+++ lesson11/lesson11.23_MadLibs/quiz2-11.js
@@ -25,3 +25,5 @@ var adjective2 = 'fun';
 var adjective3 = 'entertaining';
 
 // your code goes here
+var madLib = "The Intro to JavaScript course is " + adjective1 + ". James and Julia are so " + adjective2 + ". I cannot wait to work through the rest of this " + adjective3 + " content!";
+console.log(madLib);
