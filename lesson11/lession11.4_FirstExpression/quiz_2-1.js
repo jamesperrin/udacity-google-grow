102db8543df9db5060fe1a68909580a9a301bf40
Perrin, James <james.perrin.va@gmail.com>
2018-01-22 08:34:37 -0800

    Completed lesson 11.4.

---
+++ lesson11/lession11.4_FirstExpression/quiz_2-1.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: First Expression (2-1)
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ * Hint: +, -, *, /, and % are possible arithmetic operators
+ */
+
+// this expression equals 4, change it to equal 42
+console.log(11 + 10 - 4 + 5 * 10 / 2);
