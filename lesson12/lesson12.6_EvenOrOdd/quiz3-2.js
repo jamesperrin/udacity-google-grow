07bffb7c895dfe52708c23b303e75ffae17e854b
Perrin, James <james.perrin.va@gmail.com>
2018-01-23 07:44:53 -0800

    Completed Lesson 12.6.

--- lesson12/lesson12.6_EvenOrOdd/quiz3-2.js
+++ lesson12/lesson12.6_EvenOrOdd/quiz3-2.js
@@ -1,32 +1,32 @@
  /*
-     Directions:
+      Directions:
 
-     Write an if ...else statement that:
+      Write an if ...else statement that:
 
-     prints "even" if the number is an even number
-     prints "odd" if the number is an odd number
+      prints "even" if the number is an even number
+      prints "odd" if the number is an odd number
 
-         Hint: Use the % (modulo) operator to determine if a number is even or odd.
-         The modulo operater takes two numbers and returns the remainder when the first number is divided by the second one:
+          Hint: Use the % (modulo) operator to determine if a number is even or odd.
+          The modulo operater takes two numbers and returns the remainder when the first number is divided by the second one:
 
-         console.log(12 % 3);
-         console.log(10 % 4);
+          console.log(12 % 3);
+          console.log(10 % 4);
 
-         Result:
-         0
-         2
+          Result:
+          0
+          2
 
-         The answer
-         
-         for 12 % 3 is 0 because twelve divided by three has no remainder.
-         10 % 4 is 2 because ten divided by 4 has a remainder of two.
+          The answer
+          
+          for 12 % 3 is 0 because twelve divided by three has no remainder.
+          10 % 4 is 2 because ten divided by 4 has a remainder of two.
 
-     Make sure to test your code with different values.For example:
+      Make sure to test your code with different values.For example:
 
-     If number equals 1, then odd should be printed to the console.
-     If number equals 12, then even should be printed to the console.
+      If number equals 1, then odd should be printed to the console.
+      If number equals 12, then even should be printed to the console.
 
-    */
+     */
 
  /*
   * Programming Quiz: Even or Odd (3-2)
@@ -40,8 +40,8 @@
  // change the value of `number` to test your if...else statement
  var number = 2;
 
- if ( /* your conditional goes here */ ) {
-
+ if (number % 2 === 0) {
+     console.log('even');
  } else {
-
+     console.log('odd');
  }
