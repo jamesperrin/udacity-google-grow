bc53864d08285c64a52345bdcadbc41bd5845f10
Perrin, James <james.perrin.va@gmail.com>
2018-01-23 07:59:48 -0800

    Completed Lesson 12.7.

--- lesson12/lesson12.7_MusicalGroups/quiz3-3.js
+++ lesson12/lesson12.7_MusicalGroups/quiz3-3.js
@@ -31,3 +31,16 @@ Write a series of conditional statements that:
 var musicians = 1;
 
 // your code goes here
+if (musicians <= 0) {
+    console.log('not a group');
+} else if (musicians === 1) {
+    console.log('solo');
+} else if (musicians === 2) {
+    console.log('duet');
+} else if (musicians === 3) {
+    console.log('trio');
+} else if (musicians === 4) {
+    console.log('quartet');
+} else if (musicians > 4) {
+    console.log('this is a large group');
+}
