e8a9d48fee7fb10171b6fa473f1687a2f9bdba68
Perrin, James <james.perrin.va@gmail.com>
2018-01-23 18:59:21 -0800

    Added alternative solution.

--- lesson12/lesson12.8_MurderMystery/quiz3-4.1.js
+++ lesson12/lesson12.8_MurderMystery/quiz3-4.1.js
@@ -72,27 +72,20 @@ var suspect = "Mr. Parkes";
 var weapon = "";
 var solved = false;
 
-if (room === 'ballroom') {
+if (room === 'ballroom' && suspect === 'Mr. Kalehoff') {
     weapon = 'poison';
-    if (suspect === 'Mr. Kalehoff') {
-        solved = true;
-    }
-
-} else if (room === 'gallery') {
+    solved = true;
+} else if (room === 'gallery' && suspect === 'Ms. Van Cleve') {
     weapon = 'trophy';
-    if (suspect === 'Ms. Van Cleve') {
-        solved = true;
-    }
-} else if (room === 'billiards room') {
+    solved = true;
+} else if (room === 'billiards room' && suspect === 'Mrs. Sparr') {
     weapon = 'pool stick';
-    if (suspect === 'Mrs. Sparr') {
-        solved = true;
-    }
-} else {
+    solved = true;
+} else if (room === 'dining room' && suspect === 'Mr. Parkes') {
     weapon = 'knife';
-    if (suspect === 'Mr. Parkes') {
-        solved = true;
-    }
+    solved = true;
+} else {
+    solved = false;
 }
 
 if (solved) {
