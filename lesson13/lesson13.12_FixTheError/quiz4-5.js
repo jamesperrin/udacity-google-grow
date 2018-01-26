52671cd2ea24a318a3a8065d7eb708477923a03a
Perrin, James <james.perrin.va@gmail.com>
2018-01-26 14:25:56 -0800

    Lesson 13.12 start

---
+++ lesson13/lesson13.12_FixTheError/quiz4-5.js
@@ -0,0 +1,19 @@
+/*
+
+Directions:
+Here is a for loop that's supposed to print the numbers 5 through 9. Fix the error!
+
+for (x < 10; x++) {
+  console.log(x);
+}
+
+*/
+
+/*
+ * Programming Quiz: Fix the Error 1 (4-5)
+ */
+
+// fix the for loop
+for (x < 10; x++) {
+    console.log(x);
+}
