81159f708906b5ae1086caaaf208e6597bc6865a
Perrin, James <james.perrin.va@gmail.com>
2018-01-26 07:18:09 -0800

    General formatting.

--- lesson12/lesson12.13_IceCream/quiz3-6.js
+++ lesson12/lesson12.13_IceCream/quiz3-6.js
@@ -1,6 +1,7 @@
 /*
 Directions:
-Ice cream is one of the most versatile desserts on the planet because it can be done up so many different ways. Using logical operators, write a series of complex logical expressions that prints only if the following conditions are true:
+Ice cream is one of the most versatile desserts on the planet because it can be done up so many different ways.
+Using logical operators, write a series of complex logical expressions that prints only if the following conditions are true:
 
   * if flavor is set to vanilla or chocolate and
   * if vessel is set to cone or bowl and
