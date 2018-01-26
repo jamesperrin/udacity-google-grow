12c29638acf3c96a9bf4819dc7f88bac7daf58a1
Perrin, James <james.perrin.va@gmail.com>
2018-01-26 13:45:25 -0800

    Completed Lesson 13.6

--- lesson13/lesson13.6_CountdownLiftoff/quiz4-3.js
+++ lesson13/lesson13.6_CountdownLiftoff/quiz4-3.js
@@ -45,3 +45,24 @@ Your output should look like the following:
  */
 
 // your code goes here
+
+var countDown = 60;
+
+while (countDown >= 0) {
+    if (countDown === 50) {
+        console.log('Orbiter transfers from ground to internal power');
+    } else if (countDown === 31) {
+        console.log('Ground launch sequencer is go for auto sequence start');
+    } else if (countDown === 16) {
+        console.log('Activate launch pad sound suppression system');
+    } else if (countDown === 10) {
+        console.log('Activate main engine hydrogen burnoff system');
+    } else if (countDown === 6) {
+        console.log('Main engine start');
+    } else if (countDown === 0) {
+        console.log('Solid rocket booster ignition and liftoff!');
+    } else {
+        console.log(`T-${countDown} seconds`);
+    }
+    countDown -= 1;
+}
