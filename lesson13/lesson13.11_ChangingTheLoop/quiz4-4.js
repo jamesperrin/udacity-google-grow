0e3dc93492baf4427a7fa7c947176e103aae9738
Perrin, James <james.perrin.va@gmail.com>
2018-01-26 14:24:22 -0800

    Completed Lesson 13.11

--- lesson13/lesson13.11_ChangingTheLoop/quiz4-4.js
+++ lesson13/lesson13.11_ChangingTheLoop/quiz4-4.js
@@ -16,8 +16,7 @@ Rewrite the following while loop as a for loop:
  */
 
 // rewrite the while loop as a for loop
-var x = 9;
-while (x >= 1) {
-    console.log("hello " + x);
-    x = x - 1;
+
+for (var x = 9; x >= 1; x--) {
+    console.log(`hello ${x}`);
 }
