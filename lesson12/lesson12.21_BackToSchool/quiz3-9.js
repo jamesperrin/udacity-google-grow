b824170603db6725761315a55741a27b752bb9b9
Perrin, James <james.perrin.va@gmail.com>
2018-01-26 09:57:08 -0800

    Completed Lesson 12.21.

--- lesson12/lesson12.21_BackToSchool/quiz3-9.js
+++ lesson12/lesson12.21_BackToSchool/quiz3-9.js
@@ -45,3 +45,46 @@ var education = "no high school diploma";
 var salary;
 
 // your code goes here
+function BackToSchool(education) {
+    let salary = '';
+
+    switch (education) {
+        case 'no high school diploma':
+            salary = '$25,636';
+            break;
+
+        case 'a high school diploma':
+            salary = '$35,256';
+            break;
+
+        case 'an Associate\'s degree':
+            salary = '$41,496';
+            break;
+
+        case 'a Bachelor\'s degree':
+            salary = '$59,124';
+            break;
+
+        case 'a Master\'s degree':
+            salary = '$69,732';
+            break;
+
+        case 'a Professional degree':
+            salary = '$89,960';
+            break;
+
+        case 'a Doctoral degree':
+            salary = '$84,396';
+            break;
+    }
+
+    console.log(`In 2015, a person with ${education} earned an average of ${salary}/year.`);
+}
+
+console.log(BackToSchool(education = 'no high school diploma'));
+console.log(BackToSchool(education = 'a high school diploma'));
+console.log(BackToSchool(education = 'an Associate\'s degree'));
+console.log(BackToSchool(education = 'a Bachelor\'s degree'));
+console.log(BackToSchool(education = 'a Master\'s degree'));
+console.log(BackToSchool(education = 'a Professional degree'));
+console.log(BackToSchool(education = 'a Doctoral degree'));
