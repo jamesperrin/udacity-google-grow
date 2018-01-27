1adcf344e708eb5d9ee6372850cc40dfb1781b9d
Perrin, James <james.perrin.va@gmail.com>
2018-01-26 23:53:01 -0800

    Completed Lession 13.14

--- lesson13/lesson13.14_Factorials/quiz4-7.js
+++ lesson13/lesson13.14_Factorials/quiz4-7.js
@@ -3,7 +3,8 @@
 Directions:
 Write a for loop that prints out the factorial of the number 12:
 
-A factorial is calculated by multiplying a number by all the numbers below it. For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
+A factorial is calculated by multiplying a number by all the numbers below it.
+For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
 
   3! = 3 * 2 * 1 = 6 3!=3∗2∗1=6
   4! = 4 * 3 * 2 * 1 = 24 4!=4∗3∗2∗1=24
@@ -18,3 +19,21 @@ Save your final answer in a variable called solution and print it to the console
  */
 
 // your code goes here
+var factoral = 3;
+var solution = 1;
+
+
+
+function factoralize(factoral) {
+    let solution = 1;
+    for (var x = 1; x <= factoral; x++) {
+        solution *= x;
+    }
+
+    return solution;
+}
+
+// console.log(`${factoralize(factoral = 3)}`);
+// console.log(`${factoralize(factoral = 4)}`);
+// console.log(`${factoralize(factoral = 5)}`);
+console.log(`${factoralize(factoral = 12)}`);
