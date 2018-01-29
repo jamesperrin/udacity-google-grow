6d9e5d66eacc86e7d183a56f79267a3b8e410b5f
Perrin, James <james.perrin.va@gmail.com>
2018-01-29 13:37:19 -0800

    Lesson 14.20 - Add arrow function.

--- lesson14/lesson14.20_Laugh/quiz5-4.js
+++ lesson14/lesson14.20_Laugh/quiz5-4.js
@@ -12,10 +12,22 @@ Write an anonymous function expression that stores a function in a variable call
  * Programming Quiz: Laugh (5-4)
  */
 
+ // Normal function expression
 var laugh = function (numOfHas) {
   let has = "";
 
   for (let i = 1; i <= numOfHas; i++) { 
+    has += "ha";
+  }
+
+  return `${has}!`;
+};
+
+//ES6 arrow function function expression
+var laugh = numOfHas => {
+  let has = "";
+
+  for (let i = 1; i <= numOfHas; i++) {
     has += "ha";
   }
 
