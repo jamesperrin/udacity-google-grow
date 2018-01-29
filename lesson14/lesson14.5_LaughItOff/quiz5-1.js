f34781b8cb09c9460f3ca4ac5965c61edfd7fc8b
Perrin, James <james.perrin.va@gmail.com>
2018-01-29 09:18:14 -0800

    Lesson 14.5 start.

---
+++ lesson14/lesson14.5_LaughItOff/quiz5-1.js
@@ -0,0 +1,14 @@
+/*
+Directions:
+Declare a function called laugh() that returns "hahahahahahahahahaha!".
+Print the value returned from the laugh() function to the console.
+
+*/
+
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+// your code goes here
+
+console.log(/* call the laugh function */);
