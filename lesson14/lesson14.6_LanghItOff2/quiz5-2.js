fa5708f14305f73076f9f521e5198bc26f07abd5
Perrin, James <james.perrin.va@gmail.com>
2018-01-29 09:23:51 -0800

    Lesson 14.6 start.

---
+++ lesson14/lesson14.6_LanghItOff2/quiz5-2.js
@@ -0,0 +1,25 @@
+/*
+
+Directions:
+Write a function called laugh() that takes one parameter, num that represents the number of "ha"s to return.
+
+TIP: You might need a loop to solve this!
+
+Here's an example of the output and how to call the function that you will write:
+
+  console.log(laugh(3));
+
+  Prints: "hahaha!"
+
+*/
+
+/*
+ * Programming Quiz: Laugh it Off 2 (5-2)
+ *
+ * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+ *
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ *  - make sure that your function produces the correct results when it is called multiple times
+ */
+
