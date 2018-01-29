116d806f24eec9a82af5e2dc9fa5a0f16f735735
Perrin, James <james.perrin.va@gmail.com>
2018-01-29 13:38:23 -0800

    Lesson 14.22 start.

---
+++ lesson14/lesson14.22_Inline/quiz5-6.js
@@ -0,0 +1,23 @@
+/*
+
+Directions:
+Call the emotions() function so that it prints the output you see below, but instead of passing the laugh() function as an argument, pass an inline function expression instead.
+
+  emotions("happy", laugh(2)); // you can use your laugh function from the previous quizzes
+
+  Prints: "I am happy, haha!"
+
+*/
+
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+
+// don't change this code
+function emotions(myString, myFunc) {
+  console.log("I am " + myString + ", " + myFunc(2));
+}
+
+// your code goes here
+// call the emotions function here and pass in an
+// inline function expression
