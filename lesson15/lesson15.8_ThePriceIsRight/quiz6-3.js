cde93958c0b59eacd96ab17b1e86f37eb0a37290
Perrin, James <james.perrin.va@gmail.com>
2018-01-30 09:35:32 -0800

    Lesson 15.8 start.

---
+++ lesson15/lesson15.8_ThePriceIsRight/quiz6-3.js
@@ -0,0 +1,19 @@
+/*
+Directions:
+  Starting with this array of prices, change the prices of the 1 st, 3 rd, and 7 th elements in the array.
+
+  var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+TIP: The 1 st element of any array has an index of 0.
+
+Afterwards, print out the prices array to the console.
+
+*/
+
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+// your code goes here
