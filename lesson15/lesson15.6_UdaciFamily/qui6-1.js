f53f82cda6eca1b8aade55e8f75aa0750e9595c4
Perrin, James <james.perrin.va@gmail.com>
2018-01-30 09:12:53 -0800

    Lesson 15.6 start.

---
+++ lesson15/lesson15.6_UdaciFamily/qui6-1.js
@@ -0,0 +1,13 @@
+/*
+Directions:
+
+Create an array called udaciFamily and add "Julia", "James", and your name to the array
+Then, print the udaciFamily to the console using console.log.
+
+*/
+
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ */
+
+// your code goes here
