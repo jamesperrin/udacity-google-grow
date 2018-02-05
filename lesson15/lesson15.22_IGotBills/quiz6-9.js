7348eac0e1e170d02121a3e6b9481eb70b827e88
Perrin, James <james.perrin.va@gmail.com>
2018-02-05 09:43:08 -0800

    Completed Lesson 15.22.

--- lesson15/lesson15.22_IGotBills/quiz6-9.js
+++ lesson15/lesson15.22_IGotBills/quiz6-9.js
@@ -30,7 +30,18 @@ Returns: 1
  *  - each number must have an accuracy of two decimal points
  */
 
+ 
 var bills = [50.23, 19.12, 34.01,
     100.11, 12.15, 9.90, 29.11, 12.99,
     10.00, 99.22, 102.20, 100.10, 6.77, 2.22
 ];
+
+// ES5
+// var totals = bills.map(function (bill) { 
+//     return Number((bill * 1.15).toFixed(2));
+// });
+
+// ES6
+var totals = bills.map(bill => Number((bill * 1.15).toFixed(2)));
+
+console.log(totals);
