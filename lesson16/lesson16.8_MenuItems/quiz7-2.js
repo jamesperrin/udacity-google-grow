8bced2deec0bc7ff4b6bcb9e17aaa145739eaf04
Perrin, James <james.perrin.va@gmail.com>
2018-02-05 14:35:36 -0800

    Lesson 16.8 start.

---
+++ lesson16/lesson16.8_MenuItems/quiz7-2.js
@@ -0,0 +1,15 @@
+/*
+Directions:
+Create a breakfast object to represent the following menu item:
+
+The Lumberjack - $9.95
+eggs, sausage, toast, hashbrowns, pancakes
+The object should contain properties for the name, price, and ingredients.
+
+*/
+
+/*
+ * Programming Quiz: Menu Items (7-2)
+ */
+
+// your code goes here
