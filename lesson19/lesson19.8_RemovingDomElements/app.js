7c856a89bf8740742423ab24bac3c428de029026
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 15:39:42 -0800

    Lesson 19.8 start.

---
+++ lesson19/lesson19.8_RemovingDomElements/app.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, remove the <ul> from the first article item!
+
+You must use jQuery's remove() method.
+*/
+
+// Start with this variable!
+var articleItems;
+
+articleItems = // your code goes here!
