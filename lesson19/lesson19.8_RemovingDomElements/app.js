c27dc8e6d83fe726d244fbc1db7b6b151b75db7e
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 15:50:35 -0800

    Lesson 19.8 completed.

--- lesson19/lesson19.8_RemovingDomElements/app.js
+++ lesson19/lesson19.8_RemovingDomElements/app.js
@@ -7,4 +7,13 @@ You must use jQuery's remove() method.
 // Start with this variable!
 var articleItems;
 
-articleItems = // your code goes here!
+articleItems = $('.article-item');
+
+// Version 1
+// articleItems.first().find('ul').remove();
+
+// Version 2
+articleItems.first().children('ul').remove();
+
+// Version 3 - From video solution
+//articleItems.first().children().remove('ul');
