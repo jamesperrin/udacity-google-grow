dfaaa35cbb0df73d1fe030bbc46cb0739a61b1ba
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 10:57:02 -0800

    Lesson 19.3 start.

---
+++ lesson19/lesson19.3_TogglingClassesQuiz/app.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, remove the class 'featured' from Article #2 and add it to Article #3!
+
+You must use jQuery's toggleClass method!
+*/
+
+// don't change these variable!
+var article2, article3;
+
+// your code goes here!
