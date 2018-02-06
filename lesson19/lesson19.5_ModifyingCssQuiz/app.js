9ba2b5c2ad28d5fccfc608c6bc57a233a781776c
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 15:23:11 -0800

    Lesson 19.5 start.

---
+++ lesson19/lesson19.5_ModifyingCssQuiz/app.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, change the font-size of all the article-items to 20px!
+
+You must use jQuery's css() method!
+*/
+
+// Start with this variable!
+var articleItems;
+
+articleItems = // your code goes here!
