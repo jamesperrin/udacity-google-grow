c37ae270e27edcb6afdd52b24fb6bfb122b7a436
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 10:04:35 -0800

    Lesson 18.9 start.

---
+++ lesson18/lesson18.9_SelectByClassesQuiz/app.js
@@ -0,0 +1,8 @@
+/*
+For this quiz, use a jQuery class selector to grab all of the elements of class 'article-item' on the page!
+*/
+
+// don't change this variable!
+var articleItems;
+
+articleItems = // your code goes here!
