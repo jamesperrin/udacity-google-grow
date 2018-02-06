e645a2c07c5daec8a6e62c135156e015e8a1baf8
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 10:46:38 -0800

    Lesson 19.2 start.

---
+++ lesson19/lesson19.2_YouToggleClassQuiz/app.js
@@ -0,0 +1,8 @@
+/*
+For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
+*/
+
+// don't change this variable!
+var featuredArticle;
+
+featuredArticle = // your code starts here!
