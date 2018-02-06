c3b4d98c714396b0dd2f8c330c19377c911c3d0b
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 09:25:23 -0800

    Lesson 18.8 completed.

--- lesson18/lesson18.8_SelectByTags/app.js
+++ lesson18/lesson18.8_SelectByTags/app.js
@@ -5,4 +5,6 @@ For this quiz, use a jQuery tag selector to grab all of the <li>s on the page!
 // Start with this variable! (don't delete it!)
 var listElements;
 
-listElements = // your code goes here!
+listElements = $("li");
+
+console.log(listElements);
