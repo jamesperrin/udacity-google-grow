b2771598d311668c2f8cbe94820b7d1550eccc91
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 10:13:28 -0800

    Lesson 18.12 start.

---
+++ lesson18/lesson18.12_FiltersQuiz/app.js
@@ -0,0 +1,17 @@
+/*
+For this quiz, use articleList and DOM navigation methods to collect articleList's
+sibling <h1> (var h1), children (var kids), and parent <div>s (var parents).
+
+You must use articleList to navigate to the element(s)!
+*/
+
+// Start with these variable!
+var articleList, h1, kids, parents;
+
+articleList = $('.article-list');
+
+h1 = // Your code goes here!
+
+    kids = // Your code goes here!
+
+    parents = // Your code goes here!
