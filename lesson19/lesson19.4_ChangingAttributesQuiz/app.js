b2bdad8b3544fd8c71618f653cee27ec648e5ab8
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 15:21:18 -0800

    Lesson 19.4 completed.

--- lesson19/lesson19.4_ChangingAttributesQuiz/app.js
+++ lesson19/lesson19.4_ChangingAttributesQuiz/app.js
@@ -6,5 +6,19 @@ You must use jQuery's attr() method!
 
 // Start with this variable!
 var navList;
+// Version-1
+// navList = $('.nav-item').first();
+// navList.find('a').attr('href','#1');
 
-navList = // your code goes here!
+// Version-2
+// navList = $('.nav-list .nav-item:first-child');
+// navList.find('a').attr('href', '#1');
+
+// Version-3
+navList = $('.nav-list');
+navList.children().first().find('a').attr('href', '#1');
+
+// var firstItem = navList.children().first();
+// var link = first.find('a');
+// link.attr('href', '#1');
+
