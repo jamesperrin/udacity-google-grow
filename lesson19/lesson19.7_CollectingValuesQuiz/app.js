151d4430c740c85252f42aadaf820068c0992587
Perrin, James <james.perrin.va@gmail.com>
2018-02-06 15:30:51 -0800

    Lesson 19.7 start.

---
+++ lesson19/lesson19.7_CollectingValuesQuiz/app.js
@@ -0,0 +1,12 @@
+/*
+For this quiz, use jQuery's val method to make live changes to the 'Cool Articles' <h1>!
+
+The starter code below creates an event listener that will run any time the input changes.
+For more on events, check the instructor notes.
+*/
+
+
+$('#input').on('change', function () {
+    var val;
+    val = // Your code goes here!
+});
