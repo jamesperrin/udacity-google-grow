e88ee0ec0a4b91b98505c99a8e80325fb26794d8
Perrin, James <james.perrin.va@gmail.com>
2018-02-07 15:17:18 -0800

    Lesson 19.12 completed.

--- lesson19/lesson19.12_IteratingWithEachQuiz/app.js
+++ lesson19/lesson19.12_IteratingWithEachQuiz/app.js
@@ -2,8 +2,18 @@
 For this quiz, use jQuery's each() method to iterate through the <p>s,
 calculate the length of each one, and add each length to the end of each <p>.
 
-Also, make sure you don't change the text inside each <p> except to add the length, otherwise your
-length numbers won't be correct!
+Also, make sure you don't change the text inside each <p> except to add the length, otherwise your length numbers won't be correct!
 */
 
 // Your code goes here!
+var paragraphs = $('p');
+
+paragraphs.each(function (index) { 
+    var currentElement = $(this);
+    var text = currentElement.text();
+    var textCount = text.length;
+    currentElement.append(textCount);
+    
+    // DEBUG
+    //console.log(index + ": " + textCount);
+});
