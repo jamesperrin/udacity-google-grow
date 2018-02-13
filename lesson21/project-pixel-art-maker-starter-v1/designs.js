b326ca85823fc2ffe42143308cf457edb974eeeb
Perrin, James <james.perrin.va@gmail.com>
2018-02-13 10:17:03 -0800

    (1) Cached colorPicker as a global variable. (2) Modified changeBackgroundColor method logic. (3) Create alternatives for generating grid.

--- lesson21/project-pixel-art-maker-starter-v1/designs.js
+++ lesson21/project-pixel-art-maker-starter-v1/designs.js
@@ -3,9 +3,10 @@
  *  https://review.udacity.com/#!/rubrics/641/view
  */
 
- // Global variables
+// Global variables
 const pixelCanvas = document.getElementById('pixelCanvas');
- 
+const colorPicker = document.getElementById('colorPicker');
+
 /**
  * @name detectLeftButton
  * @description Detects if only the left mouse button is pressed in a mouse event
@@ -35,14 +36,16 @@ function detectLeftButton(evt) {
  */
 function changeBackgroundColor(element, color) {
     // Select color input
-    let colorPicker = document.getElementById('colorPicker').value;
-    
+    let newColor = '';
+
     if (color) {
-        colorPicker = color;
+        newColor = color;
+    } else {
+        newColor = colorPicker.value;
     }
 
     // Change target element backgournd color
-    element.style.backgroundColor = colorPicker;
+    element.style.backgroundColor = newColor;
 }
 
 
@@ -50,30 +53,57 @@ function changeBackgroundColor(element, color) {
  * @name makeGrid
  * @description Generates a HTML table grid.
  * 
- * @param {*} height - height of grid
- * @param {*} width - width of grid
+ * @param {number} height - height of grid
+ * @param {number} width - width of grid
  */
 function makeGrid(height, width) {
     // Your code goes here!
-    const table = document.getElementById('pixelCanvas');
     let tr = '';
     let td = '';
 
-    while (table.firstChild) {
-        table.removeChild(table.firstChild);
+    // Clear pixel canvas table
+    while (pixelCanvas.firstChild) {
+        pixelCanvas.removeChild(pixelCanvas.firstChild);
     }
 
-    // Rows
+    // Grid generator Version 1
     for (let row = 0; row < height; row++) {
-        tr = document.createElement('tr');
+        tr = pixelCanvas.insertRow(row);
         // Columns
         for (let col = 0; col < width; col++) {
-            td = document.createElement('td');
-            tr.appendChild(td);
+            tr.insertCell(col);
         }
-
-        table.appendChild(tr);
     }
+
+    // Grid generator Version 2
+    // Rows
+    // for (let row = 0; row < height; row++) {
+    //     tr = document.createElement('tr');
+    //     // Columns
+    //     for (let col = 0; col < width; col++) {
+    //         td = document.createElement('td');
+    //         tr.appendChild(td);
+    //     }
+
+    //     pixelCanvas.appendChild(tr);
+    // }
+
+    // Grid generator Version 3
+    // let rows = [];
+    // for (let row = 0; row < height; row++) {
+    //     tr = document.createElement('tr');
+    //     // Columns
+    //     for (let col = 0; col < width; col++) {
+    //         td = document.createElement('td');
+    //         tr.appendChild(td);
+    //     }
+
+    //     rows.push(tr);
+    // }
+
+    // rows.forEach((el) => {
+    //     pixelCanvas.appendChild(el);
+    // });
 }
 
 
@@ -105,7 +135,7 @@ document.querySelector('form').addEventListener('submit', function (e) {
  */
 pixelCanvas.addEventListener('click', function (e) {
     if (e.target && e.target.nodeName === 'TD') {
-        changeBackgroundColor(e.target);
+        changeBackgroundColor(e.target, colorPicker.value);
     }
 });
 
@@ -129,7 +159,7 @@ pixelCanvas.addEventListener('dblclick', function (e) {
 pixelCanvas.addEventListener('mouseover', function (e) {
     if (detectLeftButton(e)) {
         if (e.target && e.target.nodeName === 'TD') {
-            changeBackgroundColor(e.target);
+            changeBackgroundColor(e.target, colorPicker.value);
         }
     }
 });
